1a1b59633df00927e5abf2b6043fe407784b2f26
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2024-01-16 13:12:10 -0700

    Continue

---
+++ SAEG_Trigger/MetodoParaBotones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SAEG_Trigger
+{
+    public class MetodoParaBotones : TriggerAction<Button>
+    {
+        protected override async void Invoke(Button btn)
+        {
+            //await Task.Delay(1000);
+            btn.BackgroundColor = Color.DarkOrange;
+            btn.Text = "Va de núez";
+            await Task.Delay(1000);
+            btn.BackgroundColor = Color.DarkViolet;
+            btn.Text = "Haz Click Aquí";
+        }
+    }
+}
